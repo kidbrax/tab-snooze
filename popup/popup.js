d4c3c0a291bee2c021511b45f69339ff87bfc8e9
Braxton Beyer <Braxton.Beyer@Stelligent.com>
2020-01-13 23:00:41 -0500

    lint js

--- popup/popup.js
+++ popup/popup.js
@@ -1,39 +1,38 @@
-let hourSnooze = document.getElementById('hours-btn');
-let daySnooze = document.getElementById('days-btn');
-let timeInput = document.getElementById('time-input');
+const hourSnooze = document.getElementById('hours-btn')
+const daySnooze = document.getElementById('days-btn')
+const timeInput = document.getElementById('time-input')
 
-function getTimestamp(hours) {
-  let currentTime = Date.now();
-  return currentTime + hours * 60 * 60 * 1000;
+function getTimestamp (hours) {
+  return Date.now() + hours * 60 * 60 * 1000
 }
 
-function snoozeTab(hours) {
-  chrome.alarms.create('tabsAlarm', { periodInMinutes: 10 });
-  chrome.storage.local.get(['snoozedTabs'], function(results) {
+function snoozeTab (hours) {
+  chrome.alarms.create('tabsAlarm', { periodInMinutes: 10 })
+  chrome.storage.local.get(['snoozedTabs'], function (results) {
     if (results.snoozedTabs === undefined) {
-      results.snoozedTabs = [];
+      results.snoozedTabs = []
     }
-    chrome.tabs.query({ active: true, currentWindow: true }, function(tab) {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tab) {
       results.snoozedTabs.push({
         tabUrl: tab[0].url,
         title: tab[0].title,
         snoozeUntil: getTimestamp(hours)
-      });
+      })
       chrome.storage.local.set(
         { snoozedTabs: results.snoozedTabs },
-        function() {
-          chrome.tabs.remove(tab[0].id);
+        function () {
+          chrome.tabs.remove(tab[0].id)
         }
-      );
-    });
-  });
+      )
+    })
+  })
 }
 
-hourSnooze.addEventListener('click', function() {
-  snoozeTab(timeInput.value);
-});
+hourSnooze.addEventListener('click', function () {
+  snoozeTab(timeInput.value)
+})
 
-daySnooze.addEventListener('click', function() {
-  const hoursToSnooze = timeInput.value * 24;
-  snoozeTab(hoursToSnooze);
-});
+daySnooze.addEventListener('click', function () {
+  const hoursToSnooze = timeInput.value * 24
+  snoozeTab(hoursToSnooze)
+})
