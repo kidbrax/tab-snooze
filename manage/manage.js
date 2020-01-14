34c76eedb1fb433efe93b2c6993dc7a2a822f94c
Braxton Beyer <Braxton.Beyer@Stelligent.com>
2020-01-13 23:14:37 -0500

    eslint fixes

--- manage/manage.js
+++ manage/manage.js
@@ -1,40 +1,40 @@
-chrome.storage.local.get(['snoozedTabs'], function(results) {
-  results.snoozedTabs.forEach(function(tab) {
-    const main = document.getElementById('main');
-    const tabRow = document.createElement('div');
-    tabRow.setAttribute('id', 'row');
-    main.appendChild(tabRow);
-    const deleteBtn = document.createElement('button');
-    deleteBtn.setAttribute('id', 'delete-btn');
-    deleteBtn.textContent = 'delete';
-    const link = document.createElement('a');
-    link.setAttribute('href', tab.tabUrl);
+chrome.storage.local.get(['snoozedTabs'], function (results) {
+  results.snoozedTabs.forEach(function (tab) {
+    const main = document.getElementById('main')
+    const tabRow = document.createElement('div')
+    tabRow.setAttribute('id', 'row')
+    main.appendChild(tabRow)
+    const deleteBtn = document.createElement('button')
+    deleteBtn.setAttribute('id', 'delete-btn')
+    deleteBtn.textContent = 'delete'
+    const link = document.createElement('a')
+    link.setAttribute('href', tab.tabUrl)
     const snoozeUntilTime = moment(tab.snoozeUntil).format(
       'MMMM Do YYYY, h:mm a'
-    );
-    link.textContent = tab.title || tab.tabUrl;
-    const time = document.createElement('span');
-    time.textContent = ' | Snoozed until: ' + snoozeUntilTime;
-    tabRow.appendChild(deleteBtn);
-    tabRow.appendChild(link);
-    tabRow.appendChild(time);
+    )
+    link.textContent = tab.title || tab.tabUrl
+    const time = document.createElement('span')
+    time.textContent = ' | Snoozed until: ' + snoozeUntilTime
+    tabRow.appendChild(deleteBtn)
+    tabRow.appendChild(link)
+    tabRow.appendChild(time)
 
-    deleteBtn.addEventListener('click', function() {
-      deleteRow(tab, tabRow);
-    });
-  });
-});
+    deleteBtn.addEventListener('click', function () {
+      deleteRow(tab, tabRow)
+    })
+  })
+})
 
-function deleteRow(tab, tabRow) {
-  chrome.storage.local.get(['snoozedTabs'], function(results) {
-    const rowsToKeep = results.snoozedTabs.filter(function(result) {
+function deleteRow (tab, tabRow) {
+  chrome.storage.local.get(['snoozedTabs'], function (results) {
+    const rowsToKeep = results.snoozedTabs.filter(function (result) {
       if (result.tabUrl === tab.tabUrl) {
-        return tab.snoozeUntil !== result.snoozeUntil;
+        return tab.snoozeUntil !== result.snoozeUntil
       }
-      return true;
-    });
-    chrome.storage.local.set({ snoozedTabs: rowsToKeep }, function() {
-      tabRow.parentNode.removeChild(tabRow);
-    });
-  });
+      return true
+    })
+    chrome.storage.local.set({ snoozedTabs: rowsToKeep }, function () {
+      tabRow.parentNode.removeChild(tabRow)
+    })
+  })
 }
