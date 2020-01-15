836402ea81a3d8769ff5beeef60a60bfd0ce34af
Braxton Beyer <Braxton.Beyer@Stelligent.com>
2020-01-14 21:35:10 -0500

    eslint fixes

--- background.js
+++ background.js
@@ -1,21 +1,21 @@
-chrome.alarms.create('tabsAlarm', { periodInMinutes: 10 });
+chrome.alarms.create('tabsAlarm', { periodInMinutes: 10 })
 
-function returnTab() {
-  chrome.storage.local.get(['snoozedTabs'], function(result) {
+function returnTab () {
+  chrome.storage.local.get(['snoozedTabs'], function (result) {
     if (result.snoozedTabs.length === 0) {
-      chrome.alarms.clear('tabsAlarm');
-      return;
+      chrome.alarms.clear('tabsAlarm')
+      return
     }
-    result.snoozedTabs.forEach(function(tab) {
+    result.snoozedTabs.forEach(function (tab) {
       if (Date.now() > tab.snoozeUntil) {
-        chrome.tabs.create({ url: tab.tabUrl });
+        chrome.tabs.create({ url: tab.tabUrl })
       }
-    });
-    let pendingTabs = result.snoozedTabs.filter(function(tab) {
-      return tab.snoozeUntil > Date.now();
-    });
-    chrome.storage.local.set({ snoozedTabs: pendingTabs });
-  });
+    })
+    let pendingTabs = result.snoozedTabs.filter(function (tab) {
+      return tab.snoozeUntil > Date.now()
+    })
+    chrome.storage.local.set({ snoozedTabs: pendingTabs })
+  })
 }
 
-chrome.alarms.onAlarm.addListener(returnTab);
+chrome.alarms.onAlarm.addListener(returnTab)
